074cc496244075e5f90774a1da70184d646d99bd
Wil <will3will3will3@hotmail.com>
2014-07-31 21:18:48 +0100

    changed text

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ include 'lib/functions.php';
     </div>
 
     <div id="scroller">
-        <span id="static-text">This text has changed</span>
+        <span id="static-text">This text has changed again and again</span>
         <div class="scrollingtext">
             <span class="review">
                 <span class="message">
