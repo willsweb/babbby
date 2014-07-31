21fb32bcbf3ee1ba3437e7becad24b51d860b415
Edwin Phillips <hello@edwinphillips.net>
2014-07-31 22:52:03 +0100

    Commented back out scroller

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ include 'lib/functions.php';
             </button>
         </div>
     </div>
-
+<!--
     <div id="scroller">
         <span id="static-text">This text has changed</span>
         <div class="scrollingtext">
@@ -56,6 +56,6 @@ include 'lib/functions.php';
             </span>
         </div>
     </div>  
-
+-->
 </body>
 </html>
