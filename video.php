c64bd6c04cc8971bd859325880e19eb4fda686cd
Edwin Phillips <hello@edwinphillips.net>
2014-07-31 22:54:04 +0100

    Cleanup video.php

--- video.php
+++ video.php
@@ -1,10 +1,3 @@
-<?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-include 'lib/functions.php';
-?>
-
 <video id='vid' width='100%' height='100%' autoplay>
   <source src='<?php echo random_file('video/'); ?>' type='video/mp4'/>
       Get a better browser.
